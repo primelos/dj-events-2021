8b4039797db241e86709b19414df820997fa8181
Ale <alearanibar@gmail.com>
2021-05-19 23:52:59 -0700

    data displayed and routes with slug working

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import {API_URL} from '@/config/index'
 
 
-export default function Home() {
+
+
+export default function Home({ events }) {
+
 
   return (
-    <Layout className=''>
-      <Head>
-
-      </Head>
-      <h1>Home</h1>
+    <Layout>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  
+  return {
+    props: { events: events.slice(0,3) },
+    revalidate: 1,
+  }
 }
